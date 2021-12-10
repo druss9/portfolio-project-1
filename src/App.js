f1adac23a34154a6015c935c39f31af06c1622e5
druss9 <druss9@gmail.com>
2021-12-10 17:31:08 +0000

    Initial Project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+     <h2>Portfolio Project</h2>
+    );
+  }
+}
+
+export default App;
