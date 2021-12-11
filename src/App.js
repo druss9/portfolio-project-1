5ee6d6a8c9ffe0c3d0e0ac3ef390986c6f48d074
druss9 <druss9@gmail.com>
2021-12-11 09:28:56 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar';
+
 
 class App extends Component {
   render() {
     return (
-     <h2>Portfolio Project</h2>
+     <Navbar/>
     );
   }
 }
