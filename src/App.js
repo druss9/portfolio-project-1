0ba05400cf7e72cb928fb7f3ccd83bb40c52b9bc
LAPTOP-VN3SMBL0\boore <druss9@gmail.com>
2021-12-22 00:07:40 +0000

    Particles js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar";
 import Header from "./components/header";
+import Particles from "react-particles-js"
 
 
 class App extends Component {
   render() {
     return ( 
         <div>
+          <Particles 
+            className="particles-canvas"
+            params={{
+              particles: {
+                number: {
+                  value: 30,
+                  density: {
+                    enable:true,
+                    value_are:900
+                  }
+                },
+                shape:{
+                  type: "circle",
+                  stroke:{
+                    width: 6,
+                    color: "#f9ab00"
+                  }
+                }
+              } 
+            }}
+          />
           <Navbar />
           <Header />
         </div> 
