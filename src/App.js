3ebfadbd94bd7b1232edce57fb008a8c3eba473b
LAPTOP-VN3SMBL0\boore <druss9@gmail.com>
2022-08-16 20:22:19 +0100

    Update to react ts particles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Particles from "react-particles-js"
+import Particles from "react-tsparticles"
 import Navbar from "./components/navbar";
 import Header from "./components/header";
 import AboutMe from "./components/aboutMe";
@@ -13,28 +13,140 @@ import Portfolio from "./components/portfolio";
 class App extends Component {
   render() {
     return ( 
-        <div>
-          <Particles 
-            className="particles-canvas"
-            params={{
-              particles: {
-                number: {
-                  value: 30,
-                  density: {
-                    enable: true,
-                    value_area: 900
-                  }
-                },
-                shape:{
-                  type: "circle",
-                  stroke:{
-                    width: 6,
-                    color: "#f9ab00"
-                  }
+      <div>
+         
+         <Particles
+         className="particles-canvas"
+          options={{
+            particles: {
+              number: {
+                value: 30,
+                limit: 300,
+                density: {
+                  enable: true,
+                  value_area: 900
                 }
-              } 
-            }}
-          />
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 0,
+                  color: "##f9ab00"
+            },
+            polygon: {
+              nb_sides: 5
+            },
+            image: {
+              src: "images/github.svg",
+              width: 100,
+              height: 100
+            }
+          },
+          opacity: {
+            value: 0.5,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.5,
+              sync: false
+            }
+          },
+          size: {
+            value: 30,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 10,
+              size_min: 10,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: true,
+            distance: 100,
+            color: "#ffffff",
+            opacity: 1,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 3,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+            }
+          }
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onHover: {
+              enable: true,
+              mode: "bubble",
+              parallax: {
+                enable: false,
+                force: 60,
+                smooth: 10
+              }
+            },
+            onClick: {
+              enable: true,
+              mode: "push"
+            },
+            resize: true
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              lineLinked: {
+                opacity: 1
+              }
+            },
+            bubble: {
+              distance: 400,
+              size: 100,
+              duration: 2,
+              opacity: 1,
+              speed: 2
+            },
+            repulse: {
+              distance: 200
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
+        },
+        backgroundMask: {
+          enable: true,
+          cover: {
+            color: {
+              value: {
+                r: 0,
+                g: 0,
+                b: 0
+              }
+            }
+          }
+        },
+        retina_detect: true,
+        fps_limit: 60,
+        background: {
+          image: "url('https://particles.js.org/images/background3.jpg')"
+        }
+      }}
+    />
+        
           <Navbar />
           <Header />
           <AboutMe />
